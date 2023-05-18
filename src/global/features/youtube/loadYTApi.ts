d403fb1e86e4c8c685472c61adc5b9e0b1644ae7
sevastyan <foxhable@gmail.com>
2023-05-18 11:12:44 +0300

    feat: youtube feature

---
+++ src/global/features/youtube/loadYTApi.ts
@@ -0,0 +1,9 @@
+import { loadScript, waitScriptStatus } from 'features/loadScript'
+
+export async function loadYTApi() {
+	await loadScript('https://www.youtube.com/iframe_api', 'ytApi')
+
+	return waitScriptStatus('ytApi', 'ready')
+}
+
+window.onYouTubeIframeAPIReady = () => window.globalScripts.ytApi = 'ready'
---
+++ src/global/features/youtube/videoInfo.ts
@@ -0,0 +1,5 @@
+type ThumbnailQuality = 'maxresdefault' | 'sddefault' | 'mqdefault' | 'hqdefault'
+
+export function getVideoThumbnail(videoId: string, quality: ThumbnailQuality = 'maxresdefault') {
+	return `https://img.youtube.com/vi/${ videoId }/${quality}.jpg`
+}
