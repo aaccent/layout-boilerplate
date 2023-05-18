d2014c5b034b84bc3a1860fddfb4e19836ba1184
sevastyan <foxhable@gmail.com>
2023-05-18 11:12:50 +0300

    feat: obeserver feature

---
+++ src/global/features/mutationObserver.ts
@@ -0,0 +1,22 @@
+const defaultOptions: MutationObserverInit = {
+	subtree: true,
+	childList: true,
+	attributes: false,
+	characterData: false,
+}
+
+type ElementList = Element[] | NodeListOf<Element>
+type MutationCallback = (mutation: MutationRecord) => void
+
+export function createMutationObserver(
+  elements: Element | ElementList,
+  callback: MutationCallback,
+  options: MutationObserverInit = {}
+) {
+	options = Object.assign(defaultOptions, options)
+
+	const observer = new MutationObserver(mutations => mutations.forEach(callback))
+
+	if (elements instanceof Element) observer.observe(elements, options)
+	else elements.forEach(el => observer.observe(el, options))
+}
