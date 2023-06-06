90e97a1e41655d0c5645a8bbaf4c5f1da393fd38
sevastyan <foxhable@gmail.com>
2023-06-06 11:32:53 +0300

    chore: fix getEntry files path

--- webpack/getEntry.cjs
+++ webpack/getEntry.cjs
@@ -1,5 +1,4 @@
 const files = require('./files.cjs')
-const { foldersNames } = require('./paths.cjs')
 
 module.exports = async (pathsArr) => {
 	const result = {}
@@ -9,7 +8,8 @@ module.exports = async (pathsArr) => {
 
 	for (const filepath of entries) {
 		const filename = filepath.match(new RegExp(`/([\\w_-]+)\\.${ ext }`))[1]
-		result[filename] = `./src/${ filename }.pug`
+		const path = filepath.match(new RegExp(`/(src/[\\w_/-]+)/${filename}\\.${ext}`))[1]
+		result[filename] = `./${path}/${filename}.${ext}`
 	}
 
 	return result
