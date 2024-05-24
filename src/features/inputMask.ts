717a5ed0bc36499d8366eb5f0e29f3016d9f8608
sevastyan <seva@aaccent.ru>
2024-05-24 17:29:58 +0300

    fix: imask types
    Signed-off-by: sevastyan <seva@aaccent.ru>

--- src/features/inputMask.ts
+++ src/features/inputMask.ts
@@ -1,8 +1,9 @@
 import IMask from 'imask'
+import type { FactoryArg } from 'imask/masked/factory'
 
 const telInputs =  document.querySelectorAll<HTMLElement>('input[type="tel"]')
 
-const maskOptions: IMask.AnyMaskedOptions = {
+const maskOptions: FactoryArg = {
 	mask: '+{7}(000)000-00-00'
 }
 telInputs.forEach(input => IMask(input, maskOptions))
